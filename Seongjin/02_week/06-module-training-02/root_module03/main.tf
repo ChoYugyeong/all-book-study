4d4c8fa6aa1d063460c8365d31a719d474d0ea0b
이성진 <seongjin@iseongjin-ui-MacBookAir.local>
2024-10-14 00:36:27 +0900

    모듈의 반복문

---
+++ Seongjin/02_week/06-module-training-02/root_module03/main.tf
@@ -0,0 +1,26 @@
+locals {
+    env = {
+        dev = {
+            type = "t3.micro"
+            name = "dev_ec2"
+        }
+        
+        prod = {
+            type = "t3.small"
+            name ="prod_ec2"
+        }
+    }
+}
+
+module "ec2_seoul" {
+    for_each = local.env
+    source = "../modules/terraform-aws-ec2"
+    instance_type = each.value.type
+    instance_name = each.value.name
+}
+
+output "module_output" {
+    value = [
+        for k in module.ec2_seoul: k.private_ip
+    ]
+}
