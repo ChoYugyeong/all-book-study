545dff4f83570fd83e32a0167b199b546ebe5b14
ChoYugyeong <89493845+ChoYugyeong@users.noreply.github.com>
2024-12-15 13:35:35 +0900

    Delete Kyeongin directory

--- Kyeongin/note/ch6-module/modules/terraform-random-pwgen/output.tf
+++
@@ -1,8 +0,0 @@
-### output.tf ###
-output "id" {
-    value = random_pet.name.id
-}
-
-output "pw" {
-    value = nonsensitive(random_password.password.result)
-}
--- Kyeongin/note/ch6-module/modules/terraform-random-pwgen/variable.tf
+++
@@ -1,6 +0,0 @@
-### variable.tf ###
-variable "isDB" {
-    type = bool
-    default = false
-    description = "패스워드 대상의 DB 여부"
-}
